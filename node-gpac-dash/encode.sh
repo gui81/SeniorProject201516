8776a8a90839bbba3f5db69821497ecd6c35b0f4
Lucas Johnson <lucas.johnson@scientiallc.com>
2016-05-17 14:49:33 -0400

    Updated encode.sh to use 'ip addr'

--- node-gpac-dash/encode.sh
+++ node-gpac-dash/encode.sh
@@ -2,7 +2,7 @@
 
 cd /home/SeniorProject201516/node-gpac-dash/
 
-IPvar=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+IPvar=$(/sbin/ip addr | grep -A3 eth0 | grep 'inet ' | awk '{print $2}' | awk -F'/' '{print $1}')
 
 echo $IPvar
 
